2f11f54ad9928741ed07db0650de41408e28383b
roniroy2080 <148807686+roniroy2080@users.noreply.github.com>
2023-11-14 00:33:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const Server = require('http').createServer();
+const io = require('socket.io')(Server,{
+    cors: 'http://localhost',
+})
+
+const PORT = 6500
+Server.listen(PORT,()=> console.log(`We Are Live At PORT : ${PORT}`))
+
+io.on('connection', socket => {
+    console.log('a new user connected')
+
+    socket.on('offer:server', offer =>{
+        socket.broadcast.emit('offer:client',offer);
+    })
+
+    socket.on('answer:server', answer =>{
+        socket.broadcast.emit('answer:client',answer);
+    })
+
+    socket.on('nego:needed:server:offer', offer => {
+        socket.broadcast.emit('nego:needed:client:offer',offer);
+    })
+
+    socket.on('nego:answer:need:server', answer =>{
+        socket.broadcast.emit('nego:answer:need:client', answer);
+    })
+
+})
